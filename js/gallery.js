c278a09d0d56bcd7244e4d694ff85cb461461050
rogerdavid2 <rogerdavidb57@gmail.com>
2021-11-11 21:21:37 -0800

    Add files via upload

--- js/gallery.js
+++ js/gallery.js
@@ -1,24 +1,40 @@
+/* Close overlay with exit button */
+const exit = document.querySelector('.close');
 const overlay = document.querySelector('.overlay');
-overlay.addEventListener('click', close);
+exit.addEventListener('click', close);
 function close() {
     overlay.classList.remove('active');
 }
 
 /* Using querySelectorAll requires a for each loop*/
-const movers = document.querySelectorAll('.btn');
-movers.forEach(move => {
-    move.addEventListener('click', function () {
-        console.log('works');
-    })
-});
+function moveImages(rLmover) {
+    rLmover.forEach(move => {
+        move.addEventListener('click', function () {
+            console.log('moved')
+        })
+    });
+}
 
-const divButtons = document.querySelectorAll('.project');
-divButtons.forEach(button => {
-    button.addEventListener("click", () => {
-        const imageSource = button.children[0].src;
-        gImages.src = imageSource;
-        overlay.classList.add('active');
-    })
-});
+const movers = document.querySelectorAll('.btn');
+moveImages(movers);
+
+const movedImg = document.querySelector('.overlayImage');
+
+/*
+    Add an event listener to each image and 
+    obtain the url to display inside modal
+*/
+const imageButtons = document.querySelectorAll('.project');
+function displayImages(btns) {
+    btns.forEach(button => {
+        button.addEventListener("click", () => {
+            const imageSource = button.children[0].src;
+            gImages.src = imageSource;
+            overlay.classList.add('active');
+        })
+    });
+}
+
+displayImages(imageButtons);
 
 const gImages = document.querySelector('.overlayImage');
