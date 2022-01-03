60d0d419ae1ea40508af493d4cc55ef7a5446213
rogerdavid2 <rogerdavidb57@gmail.com>
2022-01-02 20:50:52 -0800

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -1,40 +1,79 @@
-/* Close overlay with exit button */
-const exit = document.querySelector('.close');
-const overlay = document.querySelector('.overlay');
-exit.addEventListener('click', close);
-function close() {
-    overlay.classList.remove('active');
+const galleryImgs = [...document.querySelectorAll('.gallery-img')];
+const lightboxImg = document.querySelector('.lightbox-img');
+const lightboxContainer = document.querySelector('.lightbox-modal');
+const lastImage = galleryImgs.length - 1;
+
+const lightboxBtns = document.querySelectorAll('.btn');
+const btnRight = document.querySelector('#right');
+const btnLeft = document.querySelector('#left');
+const btnClose = document.querySelector('.close');
+let renderImage;
+
+function showLightBox() {
+    lightboxContainer.classList.add('active');
 }
 
-/* Using querySelectorAll requires a for each loop*/
-function moveImages(rLmover) {
-    rLmover.forEach(move => {
-        move.addEventListener('click', function () {
-            console.log('moved')
-        })
-    });
+function hideLightBox() {
+    lightboxContainer.classList.remove('active');
 }
 
-const movers = document.querySelectorAll('.btn');
-moveImages(movers);
-
-const movedImg = document.querySelector('.overlayImage');
-
-/*
-    Add an event listener to each image and 
-    obtain the url to display inside modal
-*/
-const imageButtons = document.querySelectorAll('.project');
-function displayImages(btns) {
-    btns.forEach(button => {
-        button.addEventListener("click", () => {
-            const imageSource = button.children[0].src;
-            gImages.src = imageSource;
-            overlay.classList.add('active');
-        })
-    });
+function activeImage(image) {
+    lightboxImg.src = image.dataset.imagesrc;
+    renderImage = galleryImgs.indexOf(image);
+    switch (renderImage) {
+        case 0:
+            btnLeft.classList.add('inactive');
+            break
+        case lastImage:
+            btnRight.classList.add('inactive');
+            break
+        default:
+            lightboxBtns.forEach(btn => {
+                btn.classList.remove('inactive');
+            })
+    }
 }
 
-displayImages(imageButtons);
+function slideLeft() {
+    btnLeft.focus();
+    if (renderImage === 0) {
+        activeImage(galleryImgs[lastImage])
+    } else {
+        activeImage(galleryImgs[renderImage].previousElementSibling)
+    }
+}
 
-const gImages = document.querySelector('.overlayImage');
+function slideRight() {
+    btnRight.focus();
+    if (renderImage === lastImage) {
+        activeImage(galleryImgs[0])
+    } else {
+        activeImage(galleryImgs[renderImage].nextElementSibling)
+    }
+}
+
+function slide(moveItem) {
+    if (moveItem.includes('left')) {
+        slideLeft();
+    } else {
+        slideRight();
+    }
+}
+
+galleryImgs.forEach(image => {
+    image.addEventListener('click', (e) => {
+        showLightBox();
+        activeImage(image);
+    })
+})
+
+btnClose.addEventListener('click', () => {
+    hideLightBox()
+});
+
+lightboxBtns.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        e.stopPropagation();
+        slide(e.currentTarget.id);
+    })
+})
