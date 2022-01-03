31925db69d18339d118421b58abdc0cdf1139e54
rogerdavid2 <rogerdavidb57@gmail.com>
2022-01-02 21:35:23 -0800

    Add files via upload

--- js/script4.js
+++ js/script4.js
@@ -1,158 +1,35 @@
-const image = document.querySelector('img');
-const title = document.getElementById('title');
-const artist = document.getElementById('artist');
-const music = document.querySelector('audio');
-const progressContainer = document.getElementById('progress-container');
-const progress = document.getElementById('progress');
+let menuToggle = document.querySelector('.menu-toggle');
+let menuClose = document.querySelector('.menu-close');
+let menu = document.querySelector('.menu');
 
-const currentTimeL = document.getElementById('current-time');
-const durationEl = document.getElementById('duration');
+let navLinks = Array.from(document.querySelectorAll('.nav-link'));
+let images = Array.from(document.querySelectorAll('.gallery-img'));
 
-const prevBtn = document.getElementById('prev');
-const playBtn = document.getElementById('play');
-const nextBtn = document.getElementById('next');
+menuToggle.addEventListener('click', openMenu);
+menuClose.addEventListener('click', closeMenu);
 
-
-// Music 
-const songs = [
-    {
-        name: 'cirosc-1',
-        displayName: 'Remiel',
-        artist: 'cirosc'
-    },
-    {
-        name: 'cirosc-2',
-        displayName: 'Yo',
-        artist: 'cirosc'
-    },
-    {
-        name: 'cirosc-3',
-        displayName: 'moveit',
-        artist: 'cirosc'
-    },
-    {
-        name: 'cirosc-4',
-        displayName: 'controla',
-        artist: 'cirosc'
-    },
-    {
-        name: 'cirosc-5',
-        displayName: 'WEpop',
-        artist: 'cirosc'
-    },
-    {
-        name: 'cirosc-6',
-        displayName: 'wawigi',
-        artist: 'cirosc'
-    },
-    {
-        name: 'cirosc-7',
-        displayName: 'funkwav',
-        artist: 'cirosc'
-    },
-    {
-        name: 'cirosc-8',
-        displayName: 'flash',
-        artist: 'cirosc ft. lodoni'
-    },
-    {
-        name: 'cirosc-9',
-        displayName: 'WDYG',
-        artist: 'cirosc'
-    },
-    {
-        name: 'cirosc-10',
-        displayName: 'salvame',
-        artist: 'cirosc'
-    }
-];
-
-// Check if playing
-let isPlaying = false;
-
-// Play
-function playSong() {
-    isPlaying = true;
-    playBtn.classList.replace('fa-play', 'fa-pause');
-    playBtn.setAttribute('title', 'Pause');
-    music.play();
+function openMenu() {
+    menu.classList.add('active');
+    navLinks.forEach((link, idx) => {
+        setTimeout(() => {
+            link.classList.add('active');
+        }, idx * 100)
+    })
 }
 
-function pauseSong() {
-    isPlaying = false;
-    playBtn.classList.replace('fa-pause', 'fa-play');
-    playBtn.setAttribute('title', 'Play');
-    music.pause();
+function closeMenu() {
+    menu.classList.remove('active');
+    setTimeout(() => {
+        navLinks.forEach(link => {
+            link.classList.remove('active');
+        }, 100)
+    })
 }
 
-// Play or pause Event Listener
-playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
-
-function loadSong(song) {
-    title.textContent = song.displayName;
-    artist.textContent = song.artist;
-    music.src = `music/${song.name}.mp3`;
-    image.src = `img/${song.name}.jpg`
-}
-
-let songIndex = 0;
-
-function prevSong() {
-    songIndex--;
-    if (songIndex < 0) {
-        songIndex = songs.length - 1;
-    }
-    loadSong(songs[songIndex]);
-    playSong();
-}
-
-function nextSong() {
-    songIndex++;
-    if (songIndex > songs.length - 1) {
-        songIndex = 0;
-    }
-    loadSong(songs[songIndex]);
-    playSong();
-}
-
-loadSong(songs[songIndex]);
-
-function updateProgressBar(e) {
-    if (isPlaying) {
-        const { duration, currentTime } = e.srcElement;
-        const progressPercent = (currentTime / duration) * 100;
-        progress.style.width = `${progressPercent}%`;
-
-        const durationMinutes = Math.floor(duration / 60);
-        let durationSeconds = Math.floor(duration % 60);
-        if (durationSeconds < 10) {
-            durationSeconds = `0${durationSeconds}`;
-        }
-
-        if (durationSeconds) {
-            durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
-        }
-        // Calc Display
-        const currentMinutes = Math.floor(currentTime / 60);
-        let currentSeconds = Math.floor(currentTime % 60);
-        if (currentSeconds < 10) {
-            currentSeconds = `0${currentSeconds}`;
-        }
-        currentTimeL.textContent = `${currentMinutes}:${currentSeconds}`
-    }
-}
-
-function setProgressBar(e) {
-    const width = this.clientWidth;
-    const clickX = e.offsetX;
-    const { duration } = music;
-    music.currentTime = ((clickX / width) * duration);
-}
-
-
-// Event Listener
-prevBtn.addEventListener('click', prevSong);
-nextBtn.addEventListener('click', nextSong);
-music.addEventListener('ended', nextSong);
-music.addEventListener('timeupdate', updateProgressBar);
-progressContainer.addEventListener('click', setProgressBar);
+window.addEventListener('DOMContentLoaded', function (e) {
+    images.forEach((image, idx) => {
+        setTimeout(() => {
+            image.classList.add('active');
+        }, idx * 100)
+    })
+})
